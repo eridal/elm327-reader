624c096381bf1897d0df17eab47a50520f77f78e
Martin Scotta <martinscotta@gmail.com>
2014-09-14 14:26:06 -0300

    cleaner `Channel`

--- src/elm327/reader/Channel.java
+++ src/elm327/reader/Channel.java
@@ -6,9 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import com.google.common.base.Strings;
-import com.google.common.base.Throwables;
-
 class Channel {
 
     private final InputStream reader;
@@ -19,9 +16,8 @@ class Channel {
         this.writter = writter;
     }
 
-    private void write(Command c) throws IOException {
+    private void write(String message) throws IOException {
         flush();
-        String message = c.toCommandString();
         writter.write(message.getBytes());
         writter.write('\r');
         writter.flush();
@@ -29,7 +25,6 @@ class Channel {
 
     private String read() throws IOException {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
         for (;;) {
             if (reader.available() == 0 && buffer.size() > 0) {
                 break;
@@ -50,54 +45,17 @@ class Channel {
         return buffer.toString();
     }
 
-    private void flush() {
-        try {
-            while (reader.available() > 0) {
-                int b = reader.read();
-                if (b == -1) {
-                    break;
-                }
+    private void flush() throws IOException {
+        while (reader.available() > 0) {
+            int b = reader.read();
+            if (b == -1) {
+                break;
             }
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
         }
     }
 
-    private String writeAndRead(Command cmd) {
-        try {
-            write(cmd);
-            return read();
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
-        }
-    }
-
-    private static String extractCommand(Command cmd, String result) {
-        String message = cmd.toCommandString();
-        if (result.startsWith(message)) {
-            return result.substring(message.length());
-        }
-        return result;
-    }
-
-    private static String handleResult(String result) {
-        if (Strings.isNullOrEmpty(result) ||
-            Strings.isNullOrEmpty(result = result.trim())) {
-            return null;
-        }
-        if (Result.UNKNOWN.matches(result)) {
-            return null;
-        }
-        if (Result.NO_DATA.matches(result)) {
-            return null;
-        }
-        return result;
-    }
-
-    public String send(Command cmd) {
-        String result = writeAndRead(cmd);
-        return handleResult(
-            extractCommand(cmd, result)
-        );
+    public String send(String message) throws IOException {
+        write(message);
+        return read();
     }
 }
